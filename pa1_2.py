2f59a4ed79e2e93a489fb1b9753a403ecf8a3e18
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-13 20:51:29 -0500

    Add code to demo pa1 module

---
+++ pa1_2.py
@@ -0,0 +1,8 @@
+###############################################################################
+# Name: Jason D. F. D'Oyley
+# Date: November 13, 2022
+# Assessment: Performance Assessment - Hello World and Import
+###############################################################################
+import pa1
+
+pa1.python_function()
