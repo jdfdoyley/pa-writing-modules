962861098bc78753c1312f15a8b61af6dc0510d3
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-13 20:48:51 -0500

    Add code for pa1 module

---
+++ pa1.py
@@ -0,0 +1,7 @@
+###############################################################################
+# Name: Jason D. F. D'Oyley
+# Date: November 13, 2022
+# Assessment: Performance Assessment - Hello World and Import
+###############################################################################
+def python_function():
+    print("Hello world, my name is Jason D'Oyley")
